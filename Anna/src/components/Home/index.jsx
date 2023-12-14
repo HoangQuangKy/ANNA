604176299c9e00711608edf46ec7ba43aacef7de
HoangQuangKy <kyhoang487@gmail.com>
2023-12-14 14:01:29 +0700

    first commit

---
+++ Anna/src/components/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopBar from '../Common/TopBar'
+
+function Home() {
+    return (
+        <div className='flex flex-col items-center'>
+            <TopBar></TopBar>
+        </div>
+    )
+}
+
+export default Home
