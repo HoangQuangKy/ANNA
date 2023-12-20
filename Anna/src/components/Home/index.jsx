09eed074f19e2b47c50340195d44060ba9a581d1
HoangQuangKy <kyhoang487@gmail.com>
2023-12-20 23:25:13 +0700

    Home page

--- Anna/src/components/Home/index.jsx
+++ Anna/src/components/Home/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import TopBar from '../Common/TopBar'
 import BottomBar from '../Common/BottomBar'
+import Banner from './Banner'
+import Glasses from './glasses'
+import HotTrendBanner from './HotTrendBanner'
+import Adv from './Adv'
+import Marketing from './Marketing'
+import Customer from './Customer'
 
 function Home() {
     return (
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center w-full'>
             <TopBar />
-            <BottomBar />
+            <Banner />
+            <Glasses />
+            <HotTrendBanner />
+            <Adv />
+            <Marketing />
+            <Customer />
+            {/* <BottomBar /> */}
         </div >
     )
 }
