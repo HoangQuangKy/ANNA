970ee5896d999253444faa401db8aee31e0b5690
tuan.tran <anhkim280594@gmail.com>
2024-01-13 09:56:45 +0700

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,10 +3,11 @@ import BottomBar from "./Common/BottomBar"
 import { ToastContainer } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
 // import { useAuth } from "../context/auth";
-const Layout = ({ children }) => {
+
+const Layout = ({ children }, style) => {
     // const [auth, setAuth] = useAuth()
     return (
-        <div className="ml-20">
+        <div className={style? style : "ml-20"}>
             <TopBar />
             <main>
                 {children}
